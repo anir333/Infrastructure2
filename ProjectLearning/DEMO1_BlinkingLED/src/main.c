8ea20df9efdd0dfc172ea2a2ec8bba4caae61d7d
Anir Aoulad Haddou Saddik <anir.aouladhaddousaddik@student.kdg.be>
2024-04-26 21:00:51 +0200

    Should work

--- ProjectLearning/DEMO1_BlinkingLED/src/main.c
+++ ProjectLearning/DEMO1_BlinkingLED/src/main.c
@@ -2,11 +2,11 @@
 #include <util/delay.h> //Library containing the delay (~sleep) function
 
 int main(){ 
-//  DDRB = 0b00010000;  //The 3rd led is connected to PB4, numbered from right to left (start counting from 0)
+//  DDRB = 0b00011100;  //The 3rd led is connected to PB4, numbered from right to left (start counting from 0)
  while (1){          //A while 1 loop to blink "forever"  
    PORTB = 0b00000000;//we don't apply voltage to PB4, so there is a voltage difference, by consequence the led lights up
    _delay_ms(1000);
-   PORTB = 0b00010000;//we apply voltage to PB4, so the voltage difference becomes 0, by consequence the led goes dark
+   PORTB = 0b00011100;//we apply voltage to PB4, so the voltage difference becomes 0, by consequence the led goes dark
    _delay_ms(1000);
    }
 // return 0;
