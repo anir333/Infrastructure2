4fd95fdfdfb82d197f9c82f0bfbbe2e06373752b
Anir Aoulad Haddou Saddik <anir.aouladhaddousaddik@student.kdg.be>
2024-06-06 21:18:15 +0200

    JavaFX Communication With Serial Port USART

--- WEEK 4/Week 4 Project - Lunar Lander/src/main.c
+++ WEEK 4/Week 4 Project - Lunar Lander/src/main.c
@@ -3,62 +3,41 @@
 #include <util/delay.h>
 #include "display.h"
 #include "usart.h"
+#include <button.h>
 
 volatile int counter = 0;
-volatile int secondsPassed = 0;
+volatile int meters = 600;
+volatile int speed = 100;
+volatile int fuel = 1500;
+
 #define MULTIPLE 250
 
+
+ISR ( PCINT1_vect ) {
+    if(buttonPushed(1)) {
+        speed = 50;    
+    }
+}
+
 int main() {
-    initTimer();
     initDisplay();
     turnDisplayOFF();
-    startTimer();
+    enableAllButtons();
+    enableAllButtonInterrupts();
     initUSART();
 
     while (1) {
-        // Your main loop code
-        // _delay_ms(1000);  // Avoid using delay in a timing-critical loop
-    }
+        writeNumberAndWait( meters, 1000 );
+        meters -= speed;
+        if ((meters <= 100) && (speed = 100)) {
+            printf("\n\n\nThe Lunar Lander Crashed!\n\n\n");
+            break;
+        } else if ((meters <= 100) && (speed = 50)) {
+            printf("\n\n\nThe Lunar Lander Landed Safely!\n\n\n");
+            break;
+        }
+        if (buttonPushed(1)) speed = 50; else speed = 100;
+    };
 
     return 0;
-}
-
-void initTimer() {
-    // Set Timer/Counter Control Register A (TCCR2A) to CTC mode
-    TCCR2A |= _BV(WGM21);  // Set WGM21 to 1, enabling CTC mode
-    
-    // Enable the Output Compare Match A interrupt for Timer 2
-    TIMSK2 |= (1 << OCIE2A); // Set OCIE2A to 1
-    
-    OCR2A = 249;  // Compare match value for 4ms interval (assuming 16MHz clock and prescaler 64)
-    
-    // Enable global interrupts
-    sei();    
-}
-
-// This ISR runs every 4 ms
-ISR(TIMER2_COMPA_vect) {
-    // Increment the counter with 1
-    counter++;
-    
-    // Write the number to the display
-    if (counter % 2 == 0) {
-      writeNumber(secondsPassed);
-      turnDisplayOFF();
-    }
-    // If the counter is divisible by MULTIPLE, then count 1 sec
-    if ((counter % MULTIPLE) == 0) { 
-        secondsPassed++;
-        printf("\nseconds: %d\n", secondsPassed);
-    }
-}
-
-void startTimer() {
-    // Set the prescaler to 64 and start the timer
-    TCCR2B |= _BV(CS22) | _BV(CS21);
-}
-
-void stopTimer() {
-    // Clear the prescaler bits to stop the timer
-    TCCR2B &= ~(_BV(CS22) | _BV(CS21) | _BV(CS20));
 }
